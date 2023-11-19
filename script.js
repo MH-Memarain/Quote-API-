7bb6b96eafccbb0c5f20401b81c82cdc0c5628ea
MH.Memmarian <MHossien29782@gmail.com>
2023-11-19 09:37:38 -0800

    js code is done

---
+++ script.js
@@ -0,0 +1,23 @@
+// bind element 
+const btn_genarate = document.getElementById("genarate");
+const title = document.getElementById("number_qute");
+const quote = document.getElementById("quote");
+// url in adviceslip => api
+const url = 'https://api.adviceslip.com/advice';
+// method get random quote in api
+const Generate_Text =()=>{
+    fetch(url)
+    .then(res => {
+        return res.json()
+    })
+    .then(res=>{
+        return res.slip
+    })
+    .then(res=>{
+        title.innerHTML = 'ADVICE # ' + res.id
+        quote.innerHTML = res.advice  
+        console.log(res.advice)
+    })
+}
+// call Generate_Text and get random quote in api
+btn_genarate.addEventListener('click', Generate_Text)
